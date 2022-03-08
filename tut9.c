355e180abd6ba7fb139ff21b7ff24dace0e75666
Aditi <aditisuman5480@gmail.com>
2022-03-09 02:15:30 +0530

    new files

---
+++ tut9.c
@@ -0,0 +1,13 @@
+// C format 
+
+#include <stdio.h>
+
+int main()
+{
+   int a = 25;
+   float b = 54.282;
+//    printf("The value of a is %d\n and the value of b is %f\n", a, b); 
+
+printf("%8.8f this" , b);
+    return 0;
+}
